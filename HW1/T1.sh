69db1adbd642b4d63a7755f7ced1dc166afd7a43
cult2rologist <aw6x4klb@gmail.com>
2022-05-30 12:11:20 +0300

    HW1 adding

---
+++ HW1/T1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/QA_classes/HW1/F2
+mkdir A1 A2 A3
+cd ~/QA_classes/HW1/F2/A1
+touch S1.txt S2.txt S3.txt S4.json S5.json
+mkdir S1 S2 S3
+ls -la #(бестолку выводить список, если терминал появляется на полсекунды, но если что, то команда тут) 
+mv S1.txt S2.txt S5.json ~/QA_classes/HW1/F2/A1/S1
