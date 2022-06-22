c0f6c671ce13877e5dcbec4d388801a4638bc7e2
unknown <aw6x4klb@gmail.com>
2022-06-22 14:26:05 +0300

    JS2 & JS3_HW

--- JS_HW_2/JS_HW_2.js
+++ JS_HW_2/JS_HW_2.js
@@ -1,25 +1,26 @@
 // 1. Написать скриптик, который сосчитает и выведет результат от возведения 2 в степень 10, начиная со степени 1
-// const base = 2;
-// powArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// for (i = 0; i < powArr.length; i++) {
-//   result = Math.pow(base, powArr[i]);
-//   console.log(result);
+// for (const pow of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
+// console.log(Math.pow(2, pow));
 // }
 
 // 1*. Преобразовать 1 задачу в функцию, принимающую на вход степень, в которую будет возводиться число 2
-// function pow(any) {
-//   input = prompt("Enter a number");
-//   Math.pow(base, any);
+// function powMe(pow){
+//    console.log(Math.pow(2, pow));
 // }
-// console.log(pow(7));
+// powMe (10);
 
 // 2. Написать скрипт, который выведет 5 строк в консоль таким образом, чтобы в первой строчке выводилось :), во второй :):) и так далее
-const beast = ":)";
-for (i = 1; i <= 5; i++) {
-   }
-console.log(beast);
+// for (let i = 1; i <= 5; i++) {
+//    console.log(new Array(i).fill(':)').join(''));
+// }
+
 // 2*. Преобразовать 2 задачу в функцию, принимающую на вход строку, которая и будет выводиться в консоль (как в условии смайлик), а также количество строк для вывода
-// e.g. function printSmile(stroka, numberOfRows)
+// function printSmile(value, numberOfRows) {
+// for (let i = 1; i <= numberOfRows; i++) {
+//    console.log(new Array(i).fill(value).join(''));
+// }
+// }
+// printSmile ('X',7);
 
 // 3**.  Написать функцию, которая принимает на вход слово. Задача функции посчитать и вывести в консоль, сколько в слове гласных, и сколько согласных букв.
 // В консоли: Слово (word) состоит из (число) гласных и (число) согласных букв
@@ -32,10 +33,10 @@ console.log(beast);
 // console.log(getWordStructure("Check-list"));
 
 // 4**. Написать функцию, которая проверяет, является ли слово палиндромом. Проверки: 'abba', 'Abba'
-// function isPalindrom(word) {
-//   lowerChar = word.toLowerCase();
-//   checkPalindrome = lowerChar.split("").reverse().join("");
-//   return lowerChar === checkPalindrome;
-// }
-// console.log(isPalindrom("abba"));
-// console.log(isPalindrom("Abba"));
+function isPalindrom(word) {
+  lowerChar = word.toLowerCase();
+  checkPalindrome = lowerChar.split("").reverse().join("");
+  return lowerChar === checkPalindrome;
+}
+console.log(isPalindrom("abba"));
+console.log(isPalindrom("Abba"));
